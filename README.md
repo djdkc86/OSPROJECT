eafe291cd612092234d28555eed4a9469be7d70a
Thomas Wassum <thomas.e.wassum@gmail.com>
2014-11-06 10:19:20 -1000

    Happy Nate? :P

---
+++ README.md
@@ -0,0 +1,3 @@
+OSPROJECT
+=========
+Darth Zhu is among us and wants to kill us with his Death Star of a project. Let's Get r dun
---
+++ project/src/project/Controller.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project;
+
+import java.net.*;
+import java.io.*;
+import java.util.logging.*;
+import java.util.HashSet;
+/**
+ *
+ * @author djdrty
+ */
+public class Controller extends Thread {
+    private HashSet<Integer> uniqueIDS = new HashSet<Integer>(10);
+    protected Socket vcSocket;
+    private int count = 0;
+    Logger log;
+    FileHandler fh;
+    SimpleFormatter formatter;
+    public Controller() throws IOException{
+        ServerSocket serverSocket = null;
+        logging();
+        try{
+            serverSocket = new ServerSocket(10007);
+            log.info("Connection Socket Created");
+            try{
+                while(true){
+                    log.info("waiting for connection");
+                    new Controller(serverSocket.accept());
+                }
+            }catch (IOException e)
+            {
+                log.info("Could not listen on port: 10007.");
+                System.exit(1);
+            }
+        }catch (IOException e){
+            log.info("Could not listen on port: 10007.");
+        }catch (SecurityException e){
+            e.printStackTrace();
+        }
+        
+        Socket vcSocket = null;
+        log.info("Waiting for connection.....");
+        try{
+            vcSocket = serverSocket.accept();
+        }catch (IOException e){
+            log.info("Accept failed.");
+            System.exit(1);
+        }
+        log.info("Connection successful");
+        log.info("Waiting for input......");
+        
+        PrintWriter out = new PrintWriter(vcSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(vcSocket.getInputStream()));
+        String inputLine;
+        boolean done = false;
+        while((inputLine = in.readLine()) != null){
+            log.info("Controller: "+inputLine);
+            out.println(inputLine);
+            if(inputLine.equalsIgnoreCase("finish")) break;
+        }
+        out.close();
+        in.close();
+        vcSocket.close();
+        serverSocket.close();
+    }
+    private Controller(Socket compSocket){
+        vcSocket = compSocket;
+        start();
+    }
+    public void logging() throws IOException{
+        log = Logger.getLogger("LogFile");
+        fh = new FileHandler("controllerLogFile.txt");
+        log.addHandler(fh);
+        SimpleFormatter formatter = new SimpleFormatter();
+        fh.setFormatter(formatter);
+    }
+    
+    //This still needs to be fixed. Running into nullpointerexception
+    @Override
+    public void run(){
+        while(count < 10){
+            count++;
+            log.info("Comm thread started");
+            log.info("This is computer: "+count);
+            try{
+                PrintWriter out = new PrintWriter(vcSocket.getOutputStream(),true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(vcSocket.getInputStream()));
+                String inputLine;
+                boolean done = false;
+                boolean uid = false;
+                while((inputLine = in.readLine())!= null){
+                    do{
+                        log.info("Controller: "+inputLine);
+                        uid = uniqueIDS.add(Integer.parseInt(inputLine));
+                        if(inputLine.equalsIgnoreCase("finish")) break;
+                        if(!uid){
+                            out.println("Number already in use. Choose another number.");
+                            log.info("Number already in use. Choose another number.");
+                        }
+                    }while(!uid);
+                }
+                out.close();
+                in.close();
+                vcSocket.close();
+            }catch(IOException e){
+                log.info("Could not listen on port: 10007");
+                System.exit(1);
+            }
+        }
+    }
+    public static void main(String [] args) throws IOException{
+        new Controller();
+    }
+    
+}
